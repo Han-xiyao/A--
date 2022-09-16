b865e45f24e82ab7854d158c413e4436747f20ce
Han-xiyao <113604903+Han-xiyao@users.noreply.github.com>
2022-09-17 00:24:20 +0800

    Add files via upload
    I am so sleepy.

---
+++ 源.cpp
@@ -0,0 +1,10 @@
+#include "stdio.h"
+int main()
+{
+	printf("********************\n");
+	printf("*                  *\n");
+	printf("* 大学＝大不了自学 *\n");
+	printf("*                  *\n");
+	printf("********************\n");
+	return 0;	
+}
