c11cf1bb593727d6222932b86b423c91e1ce6b26
Han-xiyao <113604903+Han-xiyao@users.noreply.github.com>
2022-10-13 20:03:58 +0800

    Add files via upload

---
+++ 实验1.cpp
@@ -0,0 +1,59 @@
+#include "stdio.h"
+/*int main()
+{
+	printf("欢迎来到辽宁师范大学！\n"); 
+	return 0;
+}
+int main()
+{
+printf("****************\n"); 
+	printf("* Hello World! *\n"); 
+	printf("****************\n"); 
+	return 0;
+}
+int main()	
+{printf("       *       \n");
+	printf("      ***     \n");
+	printf("     *****     \n");
+	printf("    *******     \n");
+	printf("     *****     \n");
+	printf("      ***     \n");
+	printf("       *       \n");
+ return 0;
+}
+int main()
+{
+		
+	int a=1;
+	printf("%d\n",a);
+	char b;
+	printf("%c\n",'b');  
+	float c=8595061345;//精确到小数点后6位  说的精确度指的是小数点前面的，float只能7位，所以7位再后面的就打的不对了，会随机出现（但是总位数跟我写的一样） 
+	printf("%f\n",c) ;  
+	double d=8595061345231452;//精确到小数点后6位  说的精确度是小数点前 16位 
+	printf("%lf\n",d) ;          
+	return 0;
+}
+
+int main()
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",a*b);
+	return 0;
+}
+int main()
+{
+	float e,f;
+	scanf("%f%f",&e,&f);
+	printf("%.3f",e+f);//%.nf就表示要保留到n位小数点 
+	return 0;
+}*/
+int main()
+{
+	double g,h;
+	scanf("%lf%lf",&g,&h);
+	printf("%lf",g+h);
+	return 0;
+}
+
